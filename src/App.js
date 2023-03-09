d574d04ba9adfac89cca22e38731044837d120f9
Doan Dang <dangdo95@students.rowan.edu>
2023-03-08 21:18:58 -0500

    Started frontend

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './App.css';
+  
+function App() {
+    return (
+        <div>
+            <nav class="navbar background">
+                <ul class="nav-list">
+                    <li><a href="#courses">Schools</a></li>
+                    <li><a href="#tutorials">Majors</a></li>
+                    <li><a href="#jobs">Students</a></li>
+                    <li><a href='#student'>Professors</a></li>
+                </ul>
+  
+                <div class="rightNav">
+                    <input type="text" name="search" id="search" />
+                    <button class="btn btn-sm">Search</button>
+                </div>
+            </nav>
+  
+            <section class="section">
+                <div class="box-main">
+                    <div class="firstHalf">
+                        <h1 class="text-big">
+                            Find Your Ideal School with Collegepedia!
+                        </h1>
+                        <p class="text-small">
+                        Collegepedia helps aspiring college students make the best university selection based
+                        on their prospective majors and interests.  Students will be able to input their schools
+                        of interest and proceed to filter by major.  Students attending schools will be able to
+                        rate and write reviews on the major of choice, departments, dormitories, clubs,
+                        faculty/staff/professor, classes and overall experience of the University of choice.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section class="section">
+                <div class="box-main">
+                    <div class="secondHalf">
+                        <h1 class="text-big" id="program">
+                            Sort Your Search!
+                        </h1>
+                        <p class="text-small">
+                            Sort your search by desired school, major, location, interests or particular
+                            University Services.  Collegepedia provides an open forum of free speech
+                            surrounding curriculum, greek life, housing, tuition, and more.
+                        </p>
+                    </div>
+                </div>
+            </section>
+           
+            <footer className="footer">
+                <p className="text-footer">
+                    Copyright ©-All rights are reserved
+                </p>
+            </footer>
+        </div>
+    )
+}
+  
+export default App
